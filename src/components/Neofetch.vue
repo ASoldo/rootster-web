77ecacabf9543ee1f1a8074012195edabbccf354
ASoldo <soldo.andrej@gmail.com>
2023-04-17 21:16:31 +0200

    Update Neofetch for mobile

--- src/components/Neofetch.vue
+++ src/components/Neofetch.vue
@@ -1,25 +1,46 @@
 <template>
-  <pre class="nf text-success">
-      <code>
-                     -`                    rootster@archiebald 
-                    .o+`                   ------------------- 
-                   `ooo/                   OS: Arch Linux x86_64 
-                  `+oooo:                  Host: B560M DS3H V2 
-                 `+oooooo:                 Kernel: 6.1.12-arch1-1 
-                 -+oooooo+:                Uptime: 3 days, 16 mins 
-               `/:-:++oooo+:               Packages: 659 (pacman) 
-              `/++++/+++++++:              Shell: zsh 5.9 
-             `/++++++++++++++:             Resolution: 2560x1440, 2560x1440 
-            `/+++ooooooooooooo/`           WM: i3 
-           ./ooosssso++osssssso+`          Theme: Adwaita [GTK2/3] 
-          .oossssso-````/ossssss+`         Icons: Adwaita [GTK2/3] 
-         -osssssso.      :ssssssso.        Terminal: gnome-terminal 
-        :osssssss/        osssso+++.       CPU: 11th Gen Intel i7-11700F (16) @ 4.800GHz 
-       /ossssssss/        +ssssooo/-       GPU: NVIDIA GeForce RTX 3070 Lite Hash Rate 
-     `/ossssso+/:-        -:/+osssso+-     Memory: 8820MiB / 15866MiB 
-    `+sso+:-`                 `.-/+oso:
-   `++:.                           `-/+/                           
-   .`                                 `/                           
-      </code>
-    </pre>
+  <div class="nf text-success flex flex-col md:flex-row">
+    <pre class="w-full md:w-1/2 text-xs text-left md:text-right">
+                  <code class="p-0 m-0">
+                    -`                    
+                   .o+`                   
+                  `ooo/                   
+                 `+oooo:                  
+                `+oooooo:                 
+                -+oooooo+:                
+              `/:-:++oooo+:               
+             `/++++/+++++++:              
+            `/++++++++++++++:             
+           `/+++ooooooooooooo/`           
+          ./ooosssso++osssssso+`          
+         .oossssso-````/ossssss+`         
+        -osssssso.      :ssssssso.        
+       :osssssss/        osssso+++.       
+      /ossssssss/        +ssssooo/-       
+    `/ossssso+/:-        -:/+osssso+-     
+   `+sso+:-`                 `.-/+oso:    
+  `++:.                           `-/+/   
+  .`                                 `/   </code>
+                </pre>
+    <pre class="w-full md:w-1/2 text-xs">
+                  <code class="p-0 m-0">
+  rootster@archiebald 
+  soldo.andrej@gmail.com
+  ------------------- 
+  OS: Arch Linux x86_64 
+  Host: B560M DS3H V2 
+  Kernel: 6.2.10-arch1-1  
+  Uptime: 0 days, 1 mins 
+  Packages: 659 (pacman) 
+  Shell: zsh 5.9 
+  Resolution: 2560x1440, 2560x1440 
+  WM: i3 
+  Theme: Adwaita [GTK2/3] 
+  Icons: Adwaita [GTK2/3] 
+  Terminal: gnome-terminal 
+  CPU: 11th Gen Intel i7-11700F (16) @ 4.800GHz 
+  GPU: NVIDIA GeForce RTX 3070 Lite Hash Rate 
+  Memory: 8820MiB / 15866MiB </code>
+                  </pre>
+  </div>
 </template>
