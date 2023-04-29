f3a1458bb715d61448b15ccaf3390410ee63ceef
ASoldo <soldo.andrej@gmail.com>
2023-04-29 16:02:25 +0200

    Update Neofetch to include terminal colors sample

--- src/components/Neofetch.vue
+++ src/components/Neofetch.vue
@@ -21,26 +21,121 @@
    `+sso+:-`                 `.-/+oso:    
   `++:.                           `-/+/   
   .`                                 `/   </code>
-                </pre>
+  </pre>
     <pre class="w-full md:w-1/2 text-xs">
-                  <code class="p-0 m-0">
-  rootster@archiebald 
-  soldo.andrej@gmail.com
-  ------------------- 
-  OS: Arch Linux x86_64 
-  Host: B560M DS3H V2 
-  Kernel: 6.2.10-arch1-1  
-  Uptime: 0 days, 1 mins 
-  Packages: 659 (pacman) 
-  Shell: zsh 5.9 
-  Resolution: 2560x1440, 2560x1440 
-  WM: i3 
-  Theme: Adwaita [GTK2/3] 
-  Icons: Adwaita [GTK2/3] 
-  Terminal: gnome-terminal 
-  CPU: 11th Gen Intel i7-11700F (16) @ 4.800GHz 
-  GPU: NVIDIA GeForce RTX 3070 Lite Hash Rate 
-  Memory: 8820MiB / 15866MiB </code>
-                  </pre>
+  <code class="p-0 m-0">
+  soldo.andrej<span class="text-white">@</span>gmail.com
+  ---------------------- 
+  OS: <span class="text-white">Arch Linux x86_64</span> 
+  Host: <span class="text-white">B560M DS3H V2</span> 
+  Kernel:<span class="text-white">6.2.13-arch1-1</span> 
+  Uptime:<span class="text-white">0 days, 1 mins</span> 
+  Packages:<span class="text-white">733 (pacman)</span> 
+  Shell:<span class="text-white">zsh 5.9</span> 
+  Resolution:<span class="text-white">2560x1440, 2560x1440</span> 
+  WM:<span class="text-white">i3</span> 
+  Theme:<span class="text-white">Adwaita [GTK2/3]</span> 
+  Icons:<span class="text-white">Adwaita [GTK2/3]</span> 
+  Terminal:<span class="text-white">gnome-terminal</span> 
+  CPU:<span class="text-white">11th Gen Intel i7-11700F (16) @ 4.800GHz</span> 
+  GPU:<span class="text-white">NVIDIA GeForce RTX 3070 Lite Hash Rate</span> 
+  Memory:<span class="text-white">420MiB / 15969MiB</span> </code>
+
+    <div class="container">
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </div>
+
+  </pre>
   </div>
 </template>
+
+<style lang="postcss">
+.container {
+  display: flex;
+  margin-left: 15px;
+}
+
+.box {
+  width: 20px;
+  height: 10px;
+}
+
+.box:nth-child(1) {
+  background-color: #073642;
+}
+
+.box:nth-child(2) {
+  background-color: #dc322f;
+}
+
+.box:nth-child(3) {
+  background-color: #46b811;
+}
+
+.box:nth-child(4) {
+  background-color: #dcab10;
+}
+
+.box:nth-child(5) {
+  background-color: #268bd2;
+}
+
+.box:nth-child(6) {
+  background-color: #268bd2;
+}
+
+.box:nth-child(7) {
+  background-color: #2aa198;
+}
+
+.box:nth-child(8) {
+  background-color: #eee8d5;
+}
+
+.box:nth-child(9) {
+  background-color: #00bcd4;
+}
+
+.box:nth-child(10) {
+  background-color: #cb4b16;
+}
+
+.box:nth-child(11) {
+  background-color: #586e75;
+}
+
+.box:nth-child(12) {
+  background-color: #657b83;
+}
+
+.box:nth-child(13) {
+  background-color: #cddc39;
+}
+
+.box:nth-child(14) {
+  background-color: #6c71c4;
+}
+
+.box:nth-child(15) {
+  background-color: #93a1a1;
+}
+
+.box:nth-child(16) {
+  background-color: #fdf6e3;
+}
+</style>
