0a7a9fcafc1ca89fdd5d0e384011d0c783f9ecc2
ASoldo <soldo.andrej@gmail.com>
2023-12-20 17:45:49 +0100

    Update for Reload Prompt to be in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <RouterView />
+  <ReloadPrompt />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import config from "@/config";
+import ReloadPrompt from "@/components/ReloadPrompt.vue";
+
 console.log("Config url", config.apiUrl);
 </script>
+
 <style lang="postcss" scoped></style>
