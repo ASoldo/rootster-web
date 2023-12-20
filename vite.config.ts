001cc8ab14d8e0a52b97156a5ce1b6fb625613ab
ASoldo <soldo.andrej@gmail.com>
2023-12-20 17:12:14 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,45 @@
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon-180x180.png",
+        "maskable-icon-512x512.png",
+      ],
+      manifest: {
+        name: "Rootster",
+        short_name: "Rootster",
+        description: "Rootster | Software Development",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
