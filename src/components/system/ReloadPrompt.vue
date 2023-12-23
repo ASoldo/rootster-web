1d6efa7994cb257b79f88ffe86853a8630abb228
ASoldo <soldo.andrej@gmail.com>
2023-12-23 02:29:04 +0100

    Update

--- src/components/system/ReloadPrompt.vue
+++ src/components/system/ReloadPrompt.vue
@@ -18,7 +18,7 @@ async function close() {
     <div class="message">
       <span v-if="offlineReady"> App ready to work offline </span>
       <span v-else>
-        New content available, click on reload button to update!
+        🚀 New content available, click on reload button to update! 💫
       </span>
     </div>
     <button v-if="needRefresh" @click="reloadPage">Reload</button>
